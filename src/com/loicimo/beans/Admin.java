659ba2840ce3d5db7b4e700cd8572b7782e6a8cb
Adrien Cheynet <Adrien.sach@hotmail.fr>
2021-04-10 22:59:52 +0200

    Création espace connexion admin

---
+++ src/com/loicimo/beans/Admin.java
@@ -0,0 +1,21 @@
+package com.loicimo.beans;
+
+public class Admin {
+	
+ 	private String pseudo;
+    private String password;
+    
+	public String getPseudo() {
+		return pseudo;
+	}
+	public void setPseudo(String pseudo) {
+		this.pseudo = pseudo;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
